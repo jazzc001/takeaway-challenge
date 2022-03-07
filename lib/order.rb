ed097d85b5b1f64f840644972790b7abe1523a29
jazzc001 <jazzcheung212@gmail.com>
2022-03-07 23:16:18 +0000

    added verify method

--- lib/order.rb
+++ lib/order.rb
@@ -4,14 +4,17 @@ class Order
     attr_accessor :dishes
 
     def initialize(dishes = [])
-        menu = Menu.new
+        @menu = Menu.new
         @dishes = dishes
+        
     end
 
     def verify
+        @total = []
         @dishes.each do
-            |i| puts menu.menu[i]
+            |i| @total << @menu.menu[i]
         end
+        @total.sum
     end
 
 end
